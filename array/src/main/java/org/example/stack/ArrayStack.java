646ef07d6bd955d9be6c1abcbe102dfef5f4e16c
xxb <xxb9527@foxmail.com>
2024-04-18 11:07:40 +0800

    优化栈测试代码

--- array/src/main/java/org/example/stack/ArrayStack.java
+++ array/src/main/java/org/example/stack/ArrayStack.java
@@ -1,17 +1,19 @@
 package org.example.stack;
 
+import java.util.Scanner;
+
 public class ArrayStack {
 
     //栈的最大容量
     private int maxSize;
     //数组模拟栈
-    private int[] stackArray;
+    private int[] stack;
     //栈顶的指针
     private int top = -1;
 
     public ArrayStack(int maxSize) {
         this.maxSize = maxSize;
-        stackArray = new int[maxSize];
+        stack = new int[maxSize];
     }
 
     public boolean isFull() {
@@ -28,33 +30,54 @@ public class ArrayStack {
             return;
         }
         top++;
-        stackArray[top] = num;
+        stack[top] = num;
     }
 
     public int pop(){
         if(isEmpty()){
             throw new RuntimeException("栈已空");
         }
-        int value = stackArray[top];
+        int value = stack[top];
         top--;
+        System.out.printf("出栈的元素为%d", value);
+        System.out.println();
         return value;
     }
 
-    public void print(){
+    public void list(){
         for (int i = top; i >= 0 ; i--) {
-            System.out.println(stackArray[i]);
+            System.out.println(stack[i]);
         }
     }
 
 
     public static void main(String[] args) {
         ArrayStack arrayStack = new ArrayStack(5);
-        arrayStack.push(1);
-        arrayStack.push(2);
-        arrayStack.push(3);
-        int pop = arrayStack.pop();
-        System.out.printf("出栈的元素为%d", pop);
-        System.out.println();
-        arrayStack.print();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String next = scanner.next();
+            switch (next){
+                case "show":
+                    arrayStack.list();
+                    break;
+                case "exit":
+                    scanner.close();
+                    return;
+                case "push":
+                    System.out.println("请输入一个值");
+                    arrayStack.push(scanner.nextInt());
+                    break;
+                case "pop":
+                    try{
+                        arrayStack.pop();
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                default:
+                    break;
+            }
+
+        }
+
     }
 }
